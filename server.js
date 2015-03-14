bd4ff274a14479cd79e137e5c1ab797c60c67624
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-03-15 00:33:27 +0800

    Set up inital server side config with node

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'html');
+
+app.get('*', function(req, res){
+    res.render('index');
+});
+
+var port = 8080;
+app.listen(port);
+console.log('Listening on port ' + port + '...' );
