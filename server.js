f2ba3f3efb0dbf39b23c5685d8c647e71688de6c
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-03-16 03:14:37 +0800

    Added route to fetch products list from database

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ var express = require('express');
 var mongoose = require('mongoose');
 var chalk = require('chalk');
 
-var setEnvConfigs = require('./server/config/config');
-var setExpressViews = require('./server/config/express');
-var setMongooseConnection = require('./server/config/mongoose');
-var setRoutes = require('./server/config/routes');
-
 var app = express();
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var setEnvConfigs = require('./server/config/config');
 var config = setEnvConfigs[env];
 
+var setExpressViews = require('./server/config/express');
 setExpressViews(app, config);
+
+var setMongooseConnection = require('./server/config/mongoose');
 setMongooseConnection(config, env);
+
+var setRoutes = require('./server/config/routes');
 setRoutes(app);
 
 app.listen(config.port);
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,19 @@
+var mongoose = require('mongoose');
+
+var Product = mongoose.model('Product');
+
 var setRoutes= function (app) {
+
+    app.get('/productsList', function(req, res){
+        Product.find({}).exec(function(err, collection){
+            res.send(collection);
+        });
+    });
+
     app.get('/partials/*', function (req, res) {
         res.render('partials/' + req.params[0]);
     });
+
 
     app.get('*', function (req, res) {
         res.render('index');
